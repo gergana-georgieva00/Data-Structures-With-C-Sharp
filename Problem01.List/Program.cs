af8b092c7b33bcc1f7979a5406a550ed5b543a2b
Gergana Georgieva <gerganagergana1@gmail.com>
2022-10-27 22:33:44 +0300

    Add project files.

---
+++ Problem01.List/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem01.List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
